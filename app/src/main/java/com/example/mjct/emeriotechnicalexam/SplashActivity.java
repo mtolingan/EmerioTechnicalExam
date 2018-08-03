194997b40462dabaea140c79e850ca9e563f71b7
Myron Tolingan <artc.tolingan@gmail.com>
2018-08-03 11:49:43 +0800

    Added splash screen

--- app/src/main/java/com/example/mjct/emeriotechnicalexam/SplashActivity.java
+++ app/src/main/java/com/example/mjct/emeriotechnicalexam/SplashActivity.java
@@ -1,5 +1,7 @@
 package com.example.mjct.emeriotechnicalexam;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,5 +10,15 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed (new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+
+                finish ();
+            }
+        }, 2000);
     }
 }
